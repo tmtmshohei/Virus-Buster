0c2409f9816af3199e6e80b59f31b27c0cfbf586
tmtmshohei <tmsh.1023@gmail.com>
2018-08-20 11:54:37 +0900

    only scripts and shader cause of deleted scenes

---
+++ Assets/scripts/WireframeForMesh.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireframeForMesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Lines, 0);
+	}
+	
+
+}
